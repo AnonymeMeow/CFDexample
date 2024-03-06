a847b9fd8e49c4c0a36a9b41a7e8b014554c8b17
Nyan <146799252+AnonymeMeow@users.noreply.github.com>
2024-03-06 14:03:45 +0800

    separate simulation model

--- run.bat
+++ run.bat
@@ -1,10 +1,12 @@
-del "%2\*.dat"
-copy "%1\*.dat" "%2\"
+cd %2
+
+del "*.dat"
+copy "%1\*.dat" "."
 
 echo See run detail at %2/outInfo.dat
 
 %2\grid.exe
 mpiexec -n 8 %2\scarf.exe
 
-copy "%2\outInfo.dat" "%1\"
-copy "%2\*_field.dat" "%1\"
+copy "outInfo.dat" "%1\"
+copy "*_field.dat" "%1\"
